8baabdced5e010b4ef7f311d1df23fe17b7cf23e
Aslbek Norqulov <aslbeknorqulov246@gmail.com>
2024-01-27 22:51:19 +0500

    leetCode example 912 Sort an Array

---
+++ src/main/java/org/example/SortAnArray/Sort.java
@@ -0,0 +1,27 @@
+package org.example.SortAnArray;
+
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] nums = {4, 2, 1, 8, 19,-1};
+        for(int i=0 ; i<nums.length; i++){
+            int minIndex=i;
+
+            for(int j=i+1; j<nums.length ; j++){
+                if(nums[minIndex]>nums[j]) {
+                    minIndex=j;
+                }
+            }
+
+            if(minIndex !=i){
+                int temp=nums[i];
+                nums[i]=nums[minIndex];
+                nums[minIndex]=temp;
+            }
+
+        }
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
